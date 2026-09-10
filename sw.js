97662bd674ed419c36f1646a0ff34782102aeea6
hectorinpainman <noscopeconpene@gmail.com>
2026-09-10 17:22:48 +0100

    Deploy single 54MB game.data build from webbuildfinal

---
+++ sw.js
@@ -0,0 +1,41 @@
+﻿const CACHE_NAME = 'balatro-pwa-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './love.js',
+  './love.wasm',
+  './favicon.ico',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('index.html') || event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
